ed86ac4be04332edc69442fde4617a50cd9b8a06
tgurlbot <64328438+tgurlbot@users.noreply.github.com>
2020-06-27 11:46:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,7 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram File convert Bot!
+This is a Telegram File convert(tg fle to streamimg file) Bot!
+
 You can convert any Telegram Document, using this bot! With custom thumbnail
      
     """
@@ -10,8 +11,8 @@ You can convert any Telegram Document, using this bot! With custom thumbnail
     DOWNLOAD_START = "trying to download"
     UPLOAD_START = "trying to upload"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/FileToStreamBot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
@@ -22,19 +23,10 @@ You can convert any Telegram Document, using this bot! With custom thumbnail
     CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
+Plan name: <b> Premium Version </b>
 Expires on: {}"""
     HELP_USER = """I can do:
-👉 <a href="https://telegram.dog/FileConvertRobot">Convert To Streamable video any telegram file</a>
---------
-
-🔥 How to deploy : <a href='https://youtu.be/zQamSjXBpJU'>Click here</a>
-
-    © Group   : @InfotelGroup
-    © Channel : @DX_BotZ 
-    © credits : @Spechide
-
-Send /me to know current plan details
+👉 <a href="https://telegram.dog/FileToStreambot">Convert To Streamable video any telegram file</a>-------------------------------------------------------------------------
 
 """
 
