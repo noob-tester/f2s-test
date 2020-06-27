31ef6031c90b7f3c96104911864f80cc7d11f986
tgurlbot <64328438+tgurlbot@users.noreply.github.com>
2020-06-27 16:37:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,10 +6,8 @@ This is a Telegram File convert (tg fle to streamimg file) Bot!
    
    ☛ <b> /help for more details......</b>
    
-   <form action="https://google.com">
-    <input type="submit" value="Go to Google" />
-</form>
-<input type="button" onclick="location.href='https://google.com';" value="Go to Google" />
+ 
+   Created by this <a herf="https://telegram.dog/oo7robot">Person</a>
    Check my other (cloned)bot ☛ <b>@MyTestBotZ</b> """
     ABS_TEXT = " Please don't be selfish."
     BANNED_USER_TEXT = "you are banned"
