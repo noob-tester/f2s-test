5cdaf9d86311cfc4ca2a210f4ae8e07dfc26f5da
tgurlbot <64328438+tgurlbot@users.noreply.github.com>
2020-06-27 12:17:51 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+--------------#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
@@ -79,17 +79,4 @@ async def start(bot, update):
                 ]
             ]
         
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
     )
